b3b653be87c7e844373c46b235e62360764c4931
Tomasz Z <40549003+ziemastz@users.noreply.github.com>
2024-03-06 20:28:40 +0100

    Updated ArchitectureTests

--- tests/eLSC.Architecture.Test/ArchitectureTests.cs
+++ tests/eLSC.Architecture.Test/ArchitectureTests.cs
@@ -2,10 +2,17 @@ namespace eLSC.Architecture.Test;
 
 public class ArchitectureTests
 {
-    
-    [Fact]
-    public void Test1()
-    {
+    private const string DomainNamespace = "eLSC.Domain";
+    private const string ApplicationNamespace = "eLSC.Application";
+    private const string InfrastructureNamespace = "eLSC.Infrastructure";
 
+    [Fact]
+    public void DomainShouldNotHaveDependencyOnOtherProject()
+    {
+        // arrange
+        var assembly = typeof(eLSC.Domain.AssemblyReference).Assembly;
+        // act
+
+        // assert
     }
 }
